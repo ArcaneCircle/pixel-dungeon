ff9950643aaf2231bb1c07b9e6a0dc793a9e07d3
adbenitez <asieldbenitez@gmail.com>
2025-07-23 23:10:43 +0200

    improve code (part 1)

---
+++ src/dungeon.ts
@@ -0,0 +1,27 @@
+import { Level } from "./levels/level.ts";
+
+export class Dungeon {
+  public static potionOfStrength = 0;
+  public static scrollsOfUpgrade = 0;
+  public static scrollsOfEnchantment = 0;
+  public static dewVial = false; // true if the dew vial can be spawned
+
+  public static challenges = 0;
+
+  public static hero: any; // TODO: create class Hero
+  public static level: Level; // TODO: not fully ported, initial value not used
+
+  public static depth = 0;
+  public static gold = 0;
+  // Reason of death
+  public static resultDescription: String;
+
+  //public static HashSet<Integer> chapters;
+
+  // Hero's field of view
+  public static visible = new Array<boolean>(Level.LENGTH); // TODO: not fully ported, initial value not used
+
+  public static nightMode = false;
+
+  //public static SparseArray<ArrayList<Item>> droppedItems;
+}
---
+++ src/levels/level.ts
@@ -0,0 +1,5 @@
+export class Level {
+  public static WIDTH = 32;
+  public static HEIGHT = 32;
+  public static LENGTH = Level.WIDTH * Level.HEIGHT;
+}
